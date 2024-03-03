44ad314d960cbbcb635b4028dcb74ac8c2c6b4f0
DiegoNasoli321 <diego.no2003@gmail.com>
2024-03-03 15:28:16 -0300

    aaa

--- envia.php
+++ envia.php
@@ -10,6 +10,7 @@ $nome = $_POST['nome'];
 $telefone = $_POST['telefone'];
 $numeros = $_POST['numeros'];
 
+
 // Conexão com o banco de dados
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -21,10 +22,10 @@ if ($conn->connect_error) {
 $sql = "SELECT * FROM valores WHERE numeros = '$numeros'";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    echo "Número escolhido indisponível.";
-}
 
+if ($result->num_rows > 0) {
+    echo "Numeros indisponiveis";
+}
    // Preparar a query para inserir os dados no banco de dados
 $sql_insert = "INSERT INTO valores (nome, telefone, numeros, imagem) VALUES ('$nome', '$telefone', '$numeros', ?)";
 $stmt = $conn->prepare($sql_insert);
@@ -36,41 +37,10 @@ if ($stmt) {
 
     // Executar a query para inserir os dados no banco de dados
     if ($stmt->execute()) {
-        echo "<!DOCTYPE html>
-        <html>
-        <head>
-            <style>
-                body {
-                    background-color: lightgreen;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    height: 100vh;
-                    margin: 0;
-                }
-                .mensagem {
-                    text-align: center;
-                    padding: 20px;
-                    background-color: white;
-                    border-radius: 10px;
-                }
-            </style>
-        </head>
-        <body>
-            <div class='mensagem'>
-                <h1>Sua mensagem foi enviada com sucesso!</h1>
-                <p>Obrigado por enviar seus dados.</p>
-            </div>
-            <script>
-                // Limpa os campos do formulário após o envio
-                document.getElementsByTagName('form')[0].reset();
-            </script>
-        </body>
-        </html>";
-    }
+        echo 'Sucesso !';}
      else {
-        echo "Erro ao inserir informação: " . $conn->error;
+        echo "Numeros ja escolhidos, escolha novamente ";
     }
-
+    
 $conn->close();
 ?>
